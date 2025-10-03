ac71ced673571ddf659daf9208969f9d345d82e3
Zupoo <gui.tijuca@gmail.com>
2025-10-03 20:12:32 -0300

    Delete # programação directory

--- # programação/codigos/main.py
+++
@@ -1,4 +0,0 @@
-print('Olá mundo')
-qtd_pepsi = 130
-preco_pepsi = 1.50
-print(qtd_pepsi * preco_pepsi)
--- # programação/modulo25/txt/teste02.py
+++
@@ -1,4 +0,0 @@
-frase = 'Qualquer frase, com mais de, uma virugla'
-
-metade1 = frase.split(',')
-print(metade1)
